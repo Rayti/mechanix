bddd3a52102e780fe3e99c4782cde27ae6ef6a51
Milosz Karlowicz <milosz.karlowicz@capgemini.com>
2022-12-01 19:09:54 +0100

    Initial commit

---
+++ src/main/java/com/mk/mechanix/MechanixApplication.java
@@ -0,0 +1,13 @@
+package com.mk.mechanix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechanixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mk/mechanix/MechanixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.mechanix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
